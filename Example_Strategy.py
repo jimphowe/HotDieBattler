4d78f3cb7981b68a6e3d76ca252a582a148aae76
Jim Howe <jim@jims-air-2.lan>
2021-12-27 23:24:11 -0500

    working version, could use some better printing and stats output

--- Example_Strategy.py
+++ Example_Strategy.py
@@ -1,7 +1,7 @@
 # Passed current turn score, all players scores, and current roll (assuming you didn't bust)
 
 # Must contain the method take_turn, which takes in these items and returns a list representing
-# the die values you would like keep
+# the die values you would like keep, and whether you are stopping or not
 
 class HotDieBot():
     def __init__(self):
@@ -9,7 +9,7 @@ class HotDieBot():
 
     # take_turn returns a list of dice values from the roll that we would like to keep
     def take_turn(self, roll, turn_score, all_scores):
-        if self.current_score > 1000:
+        if self.current_score < 1000:
             return self.get_on_the_board_strat(roll, turn_score)
         else:
             return self.normal_strat(roll, turn_score)
@@ -18,23 +18,29 @@ class HotDieBot():
     def get_on_the_board_strat(self, roll, turn_score):
         to_keep = []
         if turn_score >= 1000:
-            return to_keep
+            return [to_keep,True]
         else:
             for value in roll:
                 if value == 1 or value == 5:
                     to_keep.append(value)
-        return to_keep
+        if len(to_keep) > 0:
+            return [to_keep,False]
+        else:
+            return [to_keep,True]
 
     # Always takes 1's and 5's until 300 points
     def normal_strat(self, roll, turn_score):
         to_keep = []
         if turn_score >= 300:
-            return to_keep
+            return [to_keep,True]
         else:
             for value in roll:
                 if value == 1 or value == 5:
                     to_keep.append(value)
-        return to_keep
+        if len(to_keep) > 0:
+            return [to_keep, False]
+        else:
+            return [to_keep, True]
 
 
 
--- Game.py
+++ Game.py
@@ -1,32 +1,32 @@
-import random
+import numpy as np
 from collections import Counter
 
-# GameState: {cur_player: x, cur_scores: {a: xx, b: xx}, cur_turn_score: x, cur_roll: [x, y, ..]}
+# GameState: {players = {a: p1, b: p2}, cur_player: x, cur_scores: {a: xx, b: xx}}
 
 # Player algos are passed their current turn score, all players scores, and their current roll,
 # from which they must decide what to keep
 
 # Update this to set up a run
 # Import strategies and add them to the player mapping
-import Example_Strategy
+from Example_Strategy import HotDieBot
 
-player_1 = Example_Strategy()
-player_2 = Example_Strategy()
+player_1 = HotDieBot()
+player_2 = HotDieBot()
 
-players = {1: player_1, 2: player_2}
+players = {0: player_1, 1: player_2}
 
-# TODO is_hot_die(), count_points()
+# TODO doesnt cycle players
 class Game():
     def __init__(self):
         self.players = players
-        self.cur_player = 1
-        self.cur_scores = {1: 0, 2:0}
+        self.cur_player = 0
+        self.cur_scores = {0:0, 1:0}
 
     def game_over(self):
-        return max([self.cur_scores.values()]) < 10000
+        return max(self.cur_scores.values()) > 10000
 
     def roll_die(self, num_die):
-        return random.sample(range(1, 6), num_die)
+        return list(np.random.randint(1, 6, size=num_die))
 
     def list_contained_in(self, list1, list2):
         c1, c2 = Counter(list1), Counter(list2)
@@ -36,6 +36,11 @@ class Game():
         return True
 
     def is_hot_die(self, roll):
+        if sorted(roll) == [1,2,3,4,5,6]:
+            return True
+        if all([n == 2 for k,n in Counter(roll).items()]) and len(roll) == 6:
+            return True
+        return False
 
     def is_bust(self, roll):
         if self.is_hot_die(roll):
@@ -58,39 +63,71 @@ class Game():
             if num in kept_die and kept_die.count(num) < 3:
                 if not self.is_hot_die(kept_die):
                     return False
+        return True
 
     def count_points(self, kept_die):
+        if len(kept_die) == 0:
+            return 0
+        if self.is_hot_die(kept_die):
+            return 1500
+        c = Counter(kept_die)
+        for k,n in c.items():
+            if n > 2:
+                if k == 1:
+                    return 1000*(2**(n - 3)) + self.count_points([x for x in kept_die if x != k])
+                else:
+                    return k*100*(2**(n - 3)) + self.count_points([x for x in kept_die if x != k])
+            if k == 1:
+                return 100 * n + self.count_points([x for x in kept_die if x != k])
+            if k == 5:
+                return 50 * n + self.count_points([x for x in kept_die if x != k])
 
     def give_turn(self, player_num):
+        print("Player " + str(player_num + 1) + " is playing")
         turn_over = False
         player = self.players[player_num]
         die_left = 6
         cur_turn_score = 0
+        # Start the turn. Roll 6 die and hand it to the player (as long as you don't bust on 6!)
         cur_roll = self.roll_die(die_left)
+        print(cur_roll)
+        if self.is_bust(cur_roll):
+            turn_over = True
         while not turn_over:
             kept_die, stopping = player.take_turn(cur_roll, cur_turn_score, self.cur_scores)
+            print("Kept: " + str(kept_die))
             if self.legal_keep(kept_die, cur_roll, player_num):
                 cur_turn_score += self.count_points(kept_die)
-                die_left = die_left - len(kept_die)
-                if die_left == 0:
-                    die_left = 6
-                cur_roll = self.roll_die(die_left)
                 if self.is_bust(cur_roll):
                     turn_over = True
                 if stopping:
                     turn_over = True
                     self.cur_scores[player_num] += cur_turn_score
+                else:
+                    die_left = die_left - len(kept_die)
+                    if die_left == 0:
+                        die_left = 6
+                    cur_roll = self.roll_die(die_left)
+                    print(cur_roll)
             else:
                 print("Player " + str(player_num) + " returned an invalid keep!...")
                 print("Die kept were " + str(kept_die) + " out of " + str(cur_roll) + ", while player had " +
-                      str(self.cur_scores[player_num]) + " points.")
-
-
-
+                      str(self.cur_scores[player_num]) + " points, and player stopping: " + str(stopping))
+                turn_over = True
+        self.cur_scores[player_num] += cur_turn_score
 
     def play_game(self):
+        turn_count = 0
         while not self.game_over():
             self.give_turn(self.cur_player)
             self.cur_player = (self.cur_player + 1) % len(self.players)
-        print("Game Over!")
-        print(self.cur_scores)
+            print(self.cur_scores)
+            turn_count += 1
+        print("Game Over in " + str(turn_count) + " turns!")
+
+def main():
+    game = Game()
+    game.play_game()
+
+if __name__ == '__main__':
+    main()
